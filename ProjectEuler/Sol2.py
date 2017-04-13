2199b79ce00e8523972a60e4452b302327d161cb
Faisal Khan <fkhan79@gmail.com>
2017-04-13 22:52:24 +0500

    Updated series

--- ProjectEuler/Sol2.py
+++ ProjectEuler/Sol2.py
@@ -1,8 +1,13 @@
-objects=[]
-def fibonacci(val):
-    if val<2:
-        return val
-    else :
-        return (fibonacci(val-1)+fibonacci(val-2)
+#fibonacci
+def f(n):
+    sumup=0
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+        print(a)
+        if a%2==0:
+            sumup +=a
+    return sumup
 
-print(fibonacci(12))
+val=input("Enter the range:")
+print("Sum of Even Valued term till %s is %i" %(val,f(int(val))))
