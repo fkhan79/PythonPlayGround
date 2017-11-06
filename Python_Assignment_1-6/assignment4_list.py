f3e114db0fd290917f6fd912b8816a58e0976709
Faisal <fkhan79@gmail.com>
2017-11-06 05:17:00 +0500

    Assignment 4

--- Python_Assignment_1-6/assignment4_list.py
+++ Python_Assignment_1-6/assignment4_list.py
@@ -5,17 +5,24 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
-
+    final=[]
+    for num in nums:
+        if nums.count(num)>1:
+            final.append(num)
+            for i in range(1,nums.count(num)):
+                nums.remove(num)
+        else:
+            final.append(num)
+    return final
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+    for element in list1:
+        list2.append(element)
+    return sorted(list2)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
@@ -27,31 +34,31 @@ def linear_merge(list1, list2):
 
 
 # Simple provided test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
+# what each function returns vs. what it"s supposed to return.
 def test(got, expected):
   if got == expected:
-    prefix = ' OK '
+    prefix = " OK "
   else:
-    prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+    prefix = "  X "
+  print("%s got: %s expected: %s" % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'remove_adjacent'
+  print("remove_adjacent")
   test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
   test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
   test(remove_adjacent([]), [])
 
   print
-  print 'linear_merge'
-  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
-       ['aa', 'aa', 'aa', 'bb', 'bb'])
+  print("linear_merge")
+  test(linear_merge(["aa", "xx", "zz"], ["bb", "cc"]),
+       ["aa", "bb", "cc", "xx", "zz"])
+  test(linear_merge(["aa", "xx"], ["bb", "cc", "zz"]),
+       ["aa", "bb", "cc", "xx", "zz"])
+  test(linear_merge(["aa", "aa"], ["aa", "bb", "bb"]),
+       ["aa", "aa", "aa", "bb", "bb"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   main()
